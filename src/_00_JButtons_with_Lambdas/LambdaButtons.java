3f71edfb636443fa53a41fdc436a405fbc7a41a3
League Student <league@iMac-38.local>
2024-04-25 18:31:34 -0700

    Lambdas

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,6 +1,10 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,7 +24,30 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
-		
+		ActionListener aL = (ActionEvent e) -> {
+			JButton source = (JButton) e.getSource();
+			if (source == addNumbers) {
+				Scanner numbers = new Scanner(System.in);
+				System.out.println("Type in your 1st number");
+				int a = numbers.nextInt();
+				System.out.println("Type in your 2nd number");
+				int b = numbers.nextInt();
+				int c = a+b;
+				System.out.println(c);
+			}
+			else if (source == randNumber) {
+				Random rand = new Random();
+				int i = rand.nextInt(10);
+				System.out.println(i);
+			}
+			else {
+				System.out.println("Did you hear about the first restaurant to open on the moon?");
+				System.out.println("It had great food, but no atmosphere.");
+			}
+		};
+		addNumbers.addActionListener(aL);
+		randNumber.addActionListener(aL);
+		tellAJoke.addActionListener(aL);
 		window.setVisible(true);
 		window.pack();
 	}
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,29 +1,100 @@
 package _01_Lambda_Methods;
 
+import java.util.Random;
+
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
-	
+
 		// Here is an example of calling the printCustomMessage method with a lambda.
 		// This prints the passed in String 10 times.
-		printCustomMessage((s)->{
-			for(int i = 0; i < 10; i++) {
+		printCustomMessage((s) -> {
+			for (int i = 0; i < 10; i++) {
 				System.out.println(s);
 			}
 		}, "repeat");
-		
-		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
-		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
-		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
-		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+
+		// 2. Call the printCustonMessage method using a lambda so that the String
+		// prints backwards.
+		printCustomMessage((s) -> {
+			char[] cArr = s.toCharArray();
+			String reverse = "";
+			for (int i = cArr.length - 1; i >= 0; i--) {
+				reverse += cArr[i];
+			}
+			System.out.println(reverse);
+		}, "testing");
+		// 3. Call the printCustonMessage method using a lambda so that the String
+		// prints with a mix between upper an lower case characters.
+		printCustomMessage((s) -> {
+			Random rand = new Random();
+			char[] cArr = s.toCharArray();
+			String mixedCaps = "";
+			// StringBuilder mixed = new StringBuilder();
+			// mixed.append(s);
+			for (int i = 0; i < cArr.length; i++) {
+				int j = rand.nextInt(2);
+				if (j == 0) {
+					mixedCaps += Character.toUpperCase(cArr[i]);
+				} else {
+					mixedCaps += Character.toLowerCase(cArr[i]);
+
+				}
+			}
+			System.out.println(mixedCaps);
+		}, "testing");
+		// 4. Call the printCustonMessage method using a lambda so that the String
+		// prints with a period in between each character.
+		printCustomMessage((s) -> {
+			StringBuilder periods = new StringBuilder();
+			periods.append(s);
+			String inBetween = "";
+			for (int i = 0; i < periods.length(); i++) {
+				inBetween += periods.charAt(i);
+				inBetween += '.';
+			}
+			System.out.println(inBetween);
+		}, "testing");
+		// 5. Call the printCustonMessage method using a lambda so that the String
+		// prints without any vowels.
+		printCustomMessage((s) -> {
+			StringBuilder consonants = new StringBuilder();
+			consonants.append(s);
+			String noVowels = "";
+			for (int i = 0; i < consonants.length(); i++) {
+				switch (consonants.charAt(i)) {
+				case 'a':
+					break;
+				case 'e':
+					break;
+				case 'i':
+					break;
+				case 'o':
+					break;
+				case 'u':
+					break;
+				case 'A':
+					break;
+				case 'E':
+					break;
+				case 'I':
+					break;
+				case 'O':
+					break;
+				case 'U':
+					break;
+				default:
+					noVowels += consonants.charAt(i);
+
+				}
+
+			}
+			System.out.println(noVowels);
+		}, "testing");
+
 	}
-	
+
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
 		sp.printSpecial(value);
 	}
 }
-	
